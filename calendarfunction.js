71a972448a9926b89d87d153cbf66e3b0468dd52
tedirland <58044717+tedirland@users.noreply.github.com>
2020-02-04 20:24:01 -0600

    Add files via upload

--- calendarfunction.js
+++ calendarfunction.js
@@ -36,3 +36,41 @@ var app = {
 }
 
 app.init();
+
+
+// Ajax Calls from Ticketmaster
+
+	// queryURL is the url we'll use to query the API
+	var urlkey =  "eXrkpUbuyRrUX1qzVXjrBbOpahQJEYLI"
+	var queryURL = "http://app.ticketmaster.com/discovery/v2/events.json?apikey=" + urlkey;
+  
+	// Begin building an object to contain our API call's query parameters
+	// Set the API key
+
+	console.log(queryURL)
+
+
+// Event Listener from Page
+jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then (function(response){
+	  console.log(response)
+	  $('.event-panel').html("<h2>" + "Name: " + response._embedded.events[0].name +"</h2>" +
+	  "<h2>" + "City: " + response._embedded.events[0]._embedded.venues[0].city.name + "</h2>" +
+	  "<h2>" + "City: " + response._embedded.events[0]._embedded.venues[0].city.name + "</h2>")
+  })
+
+
+ // _embedded.events[""0""]._embedded.venues[""0""].city.name
+
+// _embedded.events[""0""]._embedded.venues[""0""].location.longitude
+
+// _embedded.events[""0""]._embedded.venues[""0""].location.latitude
+
