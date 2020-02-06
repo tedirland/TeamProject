ff80f4575304b6f64198ea7ecd5bd15173de0cb9
Ted Irland <irlandth@gmail.com>
2020-02-06 09:18:00 -0600

    Added data element to span and working AJAX call

--- calendarfunction.js
+++ calendarfunction.js
@@ -1,136 +1,166 @@
 var app = {
-	settings: {
-		container: $('.calendar'),
-		calendar: $('.front'),
-		days: $('.weeks span'),
-		form: $('.back'),
-		input: $('.back input'),
-		buttons: $('.back button')
-	},
+  settings: {
+    container: $('.calendar'),
+    calendar: $('.front'),
+    days: $('.weeks span'),
+    form: $('.back'),
+    input: $('.back input'),
+    buttons: $('.back button')
+  },
 
-	init: function() {
-		instance = this;
-		settings = this.settings;
-		this.bindUIActions();
-	},
+  init: function () {
+    instance = this;
+    settings = this.settings;
+    this.bindUIActions();
+  },
 
-	swap: function(currentSide, desiredSide) {
-		settings.container.toggleClass('flip');
+  swap: function (currentSide, desiredSide) {
+    settings.container.toggleClass('flip');
 
-		currentSide.fadeOut(900);
-		currentSide.hide();
+    currentSide.fadeOut(900);
+    currentSide.hide();
 
-		desiredSide.show();
-	},
-
-	bindUIActions: function() {
-		settings.days.on('click', function(){
-			instance.swap(settings.calendar, settings.form);
-			settings.input.focus();
-		});
-
-		settings.buttons.on('click', function(){
-			instance.swap(settings.form, settings.calendar);
-		});
-	}
+    desiredSide.show();
+  },
+  
+  bindUIActions: function () {
+    settings.days.on('click', function () {
+      const dateSpan = document.querySelector  
+      console.log($(this).attr("data-TMDate"))
+      
+      instance.swap(settings.calendar, settings.form);
+      settings.input.focus();
+    });
+    
+    settings.buttons.on('click', function () {
+      instance.swap(settings.form, settings.calendar);
+    });
+  }
 }
 
 app.init();
 
- var now = moment()
-    var month = moment().format("MMMM")
-    var date = moment().format('dddd Do')
-    console.log(month)
+var now = moment()
+var month = moment().format("MMMM")
+var date = moment().format('dddd Do')
+console.log(month)
 
-    // Month Array Using Moment
+// Month Array Using Moment
 
-    for (var i = 1; i < 12; i++) {
+for (var i = 1; i < 12; i++) {
+  
+  var increasemonthArray = [moment().format("MMMM"),
+  moment().add(1, "month").format("MMMM"),
+  moment().add(2, "month").format("MMMM"),
+  moment().add(3, "month").format("MMMM"),
+  moment().add(4, "month").format("MMMM"),
+  moment().add(5, "month").format("MMMM"),
+  moment().add(6, "month").format("MMMM"),
+  moment().add(7, "month").format("MMMM"),
+  moment().add(8, "month").format("MMMM"),
+  moment().add(9, "month").format("MMMM"),
+  moment().add(10, "month").format("MMMM"),
+  moment().add(11, "month").format("MMMM")]
+  
+}
+var currentmonthIndex = 0;
 
-      var increasemonthArray = [moment().format("MMMM"),
-      moment().add(1, "month").format("MMMM"),
-      moment().add(2, "month").format("MMMM"),
-      moment().add(3, "month").format("MMMM"),
-      moment().add(4, "month").format("MMMM"),
-      moment().add(5, "month").format("MMMM"),
-      moment().add(6, "month").format("MMMM"),
-      moment().add(7, "month").format("MMMM"),
-      moment().add(8, "month").format("MMMM"),
-      moment().add(9, "month").format("MMMM"),
-      moment().add(10, "month").format("MMMM"),
-      moment().add(11, "month").format("MMMM")]
+$(".currentMonth").html("<h2>" + increasemonthArray[currentmonthIndex] + "</h2>")
 
-    }
-    var currentmonthIndex = 0;
-
+$(".addMo").on("click", function () {
+  currentmonthIndex++;
+  if (currentmonthIndex <= 11) {
     $(".currentMonth").html("<h2>" + increasemonthArray[currentmonthIndex] + "</h2>")
-
-    $(".addMo").on("click", function () {
-      currentmonthIndex++;
-      if (currentmonthIndex <= 11) {
-      $(".currentMonth").html("<h2>" + increasemonthArray[currentmonthIndex] + "</h2>")
-        console.log(currentmonthIndex)
-      }
-
-    })
-
-    $(".subtractMo").on("click", function () {
-      currentmonthIndex--;
-      if (currentmonthIndex >= 0) {
-        $(".currentMonth").html("<h2>" + increasemonthArray[currentmonthIndex] + "</h2>")
-
-      }
-    })
-
-    $(".dayofMonth").html(date)
-    console.log(increasemonthArray)
-
-    // DOM Elements
-    
-    var searchButton = $('#run-search')
-    
-    
-    $(searchButton).on("click", function () {
-      var cityInput = $("#citySearch").val()
-      
-      console.log(cityInput)
-
-
-    })
-
-
-// Ajax Calls from Ticketmaster
-
-	// queryURL is the url we'll use to query the API
-	var urlkey =  "eXrkpUbuyRrUX1qzVXjrBbOpahQJEYLI"
-	var queryURL = "http://app.ticketmaster.com/discovery/v2/events.json?apikey=" + urlkey;
+    console.log(currentmonthIndex)
+  }
   
-	// Begin building an object to contain our API call's query parameters
-	// Set the API key
+})
 
-	console.log(queryURL)
+$(".subtractMo").on("click", function () {
+  currentmonthIndex--;
+  if (currentmonthIndex >= 0) {
+    $(".currentMonth").html("<h2>" + increasemonthArray[currentmonthIndex] + "</h2>")
+    
+  }
+})
+
+$(".dayofMonth").html(date)
+console.log(increasemonthArray)
+
+// DOM Elements
 
 
-// Event Listener from Page
-jQuery.ajaxPrefilter(function(options) {
-    if (options.crossDomain && jQuery.support.cors) {
-        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-    }
-});
+var searchButton = $('#run-search')
+
+function buildQueryURL() {
+  var queryURL = "http://app.ticketmaster.com/discovery/v2/events.json?size=20"
+  var searchTerms = ["&"]
+  var apikey = "apikey=eXrkpUbuyRrUX1qzVXjrBbOpahQJEYLI"
+  var city = "city=" +
+  $('.citySearch')
+  .val()
+  .trim()
+  .toLowerCase()
   
+  // var date = 
+  
+  var newQueryUrl = queryURL + searchTerms + apikey + searchTerms + city
+
+  
+  var sports = document.getElementById("sportsCheck")
+  var music = document.getElementById("musicCheck")
+  var theater = document.getElementById("theaterCheck")
+  
+  console.log(sports.checked)
+  console.log(music.checked)
+  console.log(theater.checked)
+  
+  if (sports.checked) {
+    
+    newQueryUrl += "&keyword=sports"
+  } if (music.checked) {
+    
+    newQueryUrl += "&keyword=music"
+  } if (theater.checked) {
+    
+    newQueryUrl += "&keyword=theater"
+  }
+  console.log(newQueryUrl)
+  return newQueryUrl;
+  
+  console.log(newUrl)
+  // return queryURL + searchTerms + $.param(queryParams)
+  
+  
+  
+}
+
+$(".searchButton").on("click", function () {
+  
+  var queryURL = buildQueryURL();
   $.ajax({
+    type: "GET",
     url: queryURL,
-    method: "GET"
-  }).then (function(response){
-	  console.log(response)
-	  $('.event-panel').html("<h2>" + "Name: " + response._embedded.events[0].name +"</h2>" +
-	  "<h2>" + "City: " + response._embedded.events[0]._embedded.venues[0].city.name + "</h2>" +
-	  "<h2>" + "City: " + response._embedded.events[0]._embedded.venues[0].city.name + "</h2>")
-  })
+    async: true,
+    dataType: "json",
+    success: function (json) {
+      console.log(json);
+      // Parse the response.
+      // Do other things.
+    },
+    error: function (xhr, status, err) {
+      // This time, we do not end up here!
+    }
+  });
+  event.preventDefault();
 
 
- // _embedded.events[""0""]._embedded.venues[""0""].city.name
 
-// _embedded.events[""0""]._embedded.venues[""0""].location.longitude
 
-// _embedded.events[""0""]._embedded.venues[""0""].location.latitude
+
+})
+
+
+
+
 
